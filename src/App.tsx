aa2beb333197e46d7ef7a605a58c1defbe9c03e9
user00777777 <misterborman@gmail.com>
2023-02-13 18:41:20 +0200

    tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+let a:number|string='10'
+
+console.log(a);
+  return (
+    <div className="App">
+   <div>1</div>
+    </div>
+  )
+}
+
+export default App
